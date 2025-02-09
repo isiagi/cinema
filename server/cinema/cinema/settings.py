4075510d0981be764f5a02e659aa9f092bec8ac3
isiagi <isiagigeofrey0@gmail.com>
2025-02-09 15:31:33 +0300

    refactor

--- server/cinema/cinema/settings.py
+++ server/cinema/cinema/settings.py
@@ -61,12 +61,37 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
 ]
 
+# CORS Settings
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://localhost:3000",
     "http://localhost:3001",
-    'http://localhost:3000',
-    'https://cinema-vmbf.onrender.com'
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:3000",
+    "https://cinema-vmbf.onrender.com"
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
 ]
 
 MIDDLEWARE.insert(3, "cinema.middlewares.Auth0Middleware")
