fa5c26ce82bb8f5fca63a1f2a8108ddb05c930f9
isiagi <isiagigeofrey0@gmail.com>
2025-02-11 14:45:19 +0300

    chore

--- server/cinema/cinema/settings.py
+++ server/cinema/cinema/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-o!0zjaxwzge&pew%1!h7)yh=n(o(kf*u4vnl_h(9$-)3!1=t9d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cinema-vmbf.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['cinema-vmbf.onrender.com', 'localhost', '127.0.0.1', 'cinema-1-f6rw.onrender.com']
 
 
 # Application definition
