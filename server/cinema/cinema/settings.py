74a776e299e9b97120d918a88731ba3866a4b90f
isiagi <isiagigeofrey0@gmail.com>
2025-04-01 12:12:08 +0300

    add allow host

--- server/cinema/cinema/settings.py
+++ server/cinema/cinema/settings.py
@@ -112,7 +112,8 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:3000",
-    "https://cinema-vmbf.onrender.com"
+    "https://cinema-vmbf.onrender.com",
+    "https://showtimes-delta.vercel.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
